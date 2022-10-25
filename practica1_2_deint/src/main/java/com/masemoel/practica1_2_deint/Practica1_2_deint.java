7b6c19914c1f28b3287dfc0e322d6a2a3fe7a4d4
masemoel <az.az.az.mjme@gmail.com>
2022-10-25 17:40:29 +0200

    DEINT: Initial push
    - Primeras prácticas.
    
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

---
+++ practica1_2_deint/src/main/java/com/masemoel/practica1_2_deint/Practica1_2_deint.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.masemoel.practica1_2_deint;
+
+/**
+ *
+ * @author Masemoel
+ */
+public class Practica1_2_deint {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //new Ventana.setVisible(true);
+    }
+}
